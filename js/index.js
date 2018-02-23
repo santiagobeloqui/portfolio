c73aca96acf0d866b85c8e862a15d6a4f8c22eea
Santiago <sabeloqui@gmail.com>
2018-02-23 19:56:46 -0300

    primera version

---
+++ js/index.js
@@ -0,0 +1,12 @@
+cursor = document.getElementById('cursor');
+
+setInterval(()=>{
+	if (cursor.style.visibility == 'hidden'){
+		cursor.style.visibility = 'visible';
+	} else{
+		cursor.style.visibility = 'hidden';
+	}
+	
+	
+}, 700);
+
