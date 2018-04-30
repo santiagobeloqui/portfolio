ae54a6e8593d4396382611ca7106411cebfceeca
santiagobeloqui <sabeloqui@gmail.com>
2018-04-30 20:50:36 -0300

    borrado js que había quedado de una prueba

--- js/index.js
+++ js/index.js
@@ -10,27 +10,4 @@ setInterval(()=>{
 	
 }, 700);
 
-window.onscroll = function() {
-	myFunction();
-	return;
-};
 
-/*function myFunction() {
-    if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
-        alert("Hola");
-	}
-	return;
-	
-}*/
-
-let bioTitle = document.getElementById("bio-title");
-let bioText = document.getElementById("bio-text");
-/*
-window.setTimeout(()=>{
-	bioTitle.style.visibility = "visible";
-}, 700);
-
-window.setTimeout(()=>{
-	bioText.style.visibility = "visible";
-}, 1000);
-*/
